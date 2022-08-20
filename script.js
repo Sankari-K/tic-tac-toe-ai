136ebf399679b84e49df5d99cce29243198529b4
Sankari Karthik <92105177+Sankari-K@users.noreply.github.com>
2022-08-20 18:10:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -363,13 +363,13 @@ const gameFlow = (() => {
     }
 })
 
-function isEqual(arr1, arr2) {
-    for (i = 0; i < arr1.length - 1; i++) {
-        if (arr1[i] !== arr2[i]) {
-            return false;
-        }
-    }
-    return true;
-}
+// function isEqual(arr1, arr2) {
+//     for (i = 0; i < arr1.length - 1; i++) {
+//         if (arr1[i] !== arr2[i]) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
 
 gameFlow();
